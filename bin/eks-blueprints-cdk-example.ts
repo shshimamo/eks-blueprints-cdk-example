a2b501328829f093479119b38180fb53203c17ea
hiroya.shimamoto <shshimamo@gmail.com>
2023-04-30 00:15:18 +0900

    Add team

--- bin/eks-blueprints-cdk-example.ts
+++ bin/eks-blueprints-cdk-example.ts
@@ -2,10 +2,48 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import * as blueprints from '@aws-quickstart/eks-blueprints'
+import { ArnPrincipal } from "@aws-quickstart/eks-blueprints/node_modules/aws-cdk-lib/aws-iam";
+import { ApplicationTeam, PlatformTeam } from '@aws-quickstart/eks-blueprints'
 
 const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = "ap-northeast-1";
+
+// プラットフォームチーム
+const admin = new PlatformTeam({
+  name: "admin",
+  users: [
+    new ArnPrincipal(`arn:aws:iam::${account}:user/admin`)
+  ],
+});
+// アプリケーションチーム: burnham
+const burnham = new ApplicationTeam({
+  name: "burnham",
+  users: [
+    new ArnPrincipal(`arn:aws:iam::${account}:user/admin`)
+  ],
+});
+// アプリケーションチーム: riker
+const riker = new ApplicationTeam({
+  name: "riker",
+  users: [
+    new ArnPrincipal(`arn:aws:iam::${account}:user/admin`)
+  ],
+});
+// アプリケーションチーム: geordie
+const geordie = new ApplicationTeam({
+  name: "geordie",
+  users: [
+    new ArnPrincipal(`arn:aws:iam::${account}:user/admin`)
+  ],
+});
+// アプリケーションチーム: carmen
+const carmen = new ApplicationTeam({
+  name: "carmen",
+  users: [
+    new ArnPrincipal(`arn:aws:iam::${account}:user/admin`)
+  ],
+});
 
 const addOns = [
   new blueprints.addons.VpcCniAddOn(),
@@ -17,6 +55,7 @@ const addOns = [
 
 blueprints.EksBlueprint.builder()
   .account(account)
+  .teams(admin, burnham, riker, geordie, carmen) // Team指定
   .region(region)
   .addOns(...addOns)
   .build(app, "first-eks-blueprints-cdk")
