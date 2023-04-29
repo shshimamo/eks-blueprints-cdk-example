ea94e0a79fe208f4d94339a8b779391a179163f7
hiroya.shimamoto <shshimamo@gmail.com>
2023-04-29 22:42:37 +0900

    EksBlueprint

--- bin/eks-blueprints-cdk-example.ts
+++ bin/eks-blueprints-cdk-example.ts
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { EksBlueprintsCdkExampleStack } from '../lib/eks-blueprints-cdk-example-stack';
+import * as blueprints from '@aws-quickstart/eks-blueprints'
 
 const app = new cdk.App();
-new EksBlueprintsCdkExampleStack(app, 'EksBlueprintsCdkExampleStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const account = process.env.CDK_DEFAULT_ACCOUNT;
+const region = "ap-northeast-1";
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const addOns = [
+  new blueprints.addons.VpcCniAddOn(),
+  new blueprints.addons.CoreDnsAddOn(),
+  new blueprints.addons.KubeProxyAddOn(),
+  new blueprints.addons.ArgoCDAddOn(),
+  new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+]
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '...', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+blueprints.EksBlueprint.builder()
+  .account(account)
+  .region(region)
+  .addOns(...addOns)
+  .build(app, "first-eks-blueprints-cdk")
